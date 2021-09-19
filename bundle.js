f4a82744398be576a8f49e7391ee1939b958c155
Georgi Sokolov <georgi.estonia@gmail.com>
2021-09-19 16:41:45 +0900

    class list page

--- bundle.js
+++ bundle.js
@@ -1,14 +1,14 @@
 const { Client } = require("@notionhq/client")
+require('dotenv').config()
 
-const NOTION_TOKEN = 'Bearer secret_JufIrkzsH7EJaHqd1BLL3TpaP7RfPIteoycWi9nsmON'
 const NOTION_DATABASE_ID = '81a17757cdbc4f84a83d399fa76d7230'
 
 const databaseId = process.env.NOTION_DATABASE_ID
 // Initializing a client
-const notion = new Client({ auth: process.env.NOTION_TOKEN })
+const notion = new Client({ auth: process.env.NOTION_API_TOKEN })
 
 const getDatabase = async () => {
-    const response = await notion.databases.retrieve({ database_id: NOTION_DATABASE_ID });
+    const response = await notion.databases.query({ database_id: NOTION_DATABASE_ID });
   
     console.log(response);
   };
